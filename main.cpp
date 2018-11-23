f02500642f9236779943089777ffe07f814210fd
Ali Samii <a.samii@gmail.com>
2018-11-23 15:11:56 -0600

    adding version control integration to cmake

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <string>
+#include <version.h>
 
 int main() {
+  // The version is extracted from git tag and git hash
+  std::string my_version = std::string(GIT_TAG) + "-" + GIT_COMMIT_HASH;
+  std::cout << my_version << std::endl;
+  //
   try {
     std::unique_ptr<fruit_t> a_child1 = make_base("apple");
     std::unique_ptr<fruit_t> a_child2 = make_base("orange");
