8e14128f87bcae70ab4647420d1ffd8b96737259
Ali Samii <a.samii@gmail.com>
2018-11-24 18:12:13 -0600

    Fixing tests for coverage

--- wrong_init1.cpp
+++ wrong_init1.cpp
@@ -14,8 +14,6 @@ int main() {
   try {
     std::unique_ptr<fruit_t> a_child1 = make_base("apple");
     std::unique_ptr<fruit_t> a_child2 = make_base("kiwi");
-    a_child1->get_my_name(std::cout);
-    a_child2->get_my_name(std::cout);
   } catch (const std::out_of_range &) {
     std::cout << " bad initializtion !" << std::endl;
   }
